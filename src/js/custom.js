3f3c17c97855190b7695af78705ed6e6316287d2
RedX <ferz099@mail.ru>
2019-01-20 09:45:09 +0500

    Последнее обновление

--- src/js/custom.js
+++ src/js/custom.js
@@ -10,26 +10,26 @@ $('input[type=tel]').mask("+7 (000) 000 00 00");
 
 // Плавный скол с навигации
 
-/* $(".scrollto > a").click(function () {
+ $(".navigation__link").click(function () {
   var elementClick = $(this).attr("href")
   var destination = $(elementClick).offset().top;
   jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
   return false;
-}); */
+});
 
 // Плавный скол с навигации
 
 $(".licenses__slider").owlCarousel({
   loop: true,
   margin: 30,
-  nav: false,
+  nav: true,
   dots: false,
   responsive:{
       0:{
           items:1
       },
-      600:{
-          items:3
+      768:{
+          items:2
       },
       1000:{
           items:4
